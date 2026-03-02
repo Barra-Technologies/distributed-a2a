7422cbac8e1d39ddbe927e419b72aad112056f14
Fabian Bell <fabian.bell@barrabytes.com>
2026-03-02 14:33:47 +0100

    Add test cases and mock servers for agent interactions

--- __init__.py
+++ __init__.py
@@ -1,17 +1,14 @@
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph_dynamodb_checkpoint import DynamoDBSaver
 
-from .distributed_a2a import load_router, registry_heart_beat, AgentRegistryClient, \
-    McpRegistryClient
-from .distributed_a2a.registry_server import load_registry, AgentRegistryLookup, McpRegistryLookup, DynamoDbAgentRegistryLookup, DynamoDbMcpRegistryLookup, InMemoryMcpRegistry, InMemoryAgentRegistry
-from .distributed_a2a.client import RoutingA2AClient
-from .distributed_a2a.server import load_app
-from .distributed_a2a.model import AgentConfig, SkillConfig, RegistryItemConfig, RegistryConfig, LLMConfig, CardConfig, \
+from distributed_a2a.registry_server import load_registry, AgentRegistryLookup, McpRegistryLookup, DynamoDbAgentRegistryLookup, DynamoDbMcpRegistryLookup, InMemoryMcpRegistry, InMemoryAgentRegistry
+from distributed_a2a.client import RoutingA2AClient
+from distributed_a2a.server import load_app
+from distributed_a2a.model import AgentConfig, SkillConfig, RegistryItemConfig, RegistryConfig, LLMConfig, CardConfig, \
     AgentItem, RouterItem, RouterConfig
 
 __all__ = [
     "load_app",
-    "load_router",
     "RoutingA2AClient",
     "load_registry",
     "AgentRegistryLookup",
@@ -27,12 +24,8 @@ __all__ = [
     "AgentItem",
     "RouterItem",
     "RouterConfig",
-    "registry_heart_beat",
-    "AgentRegistryClient",
-    "McpRegistryClient",
     "MemorySaver",
     "DynamoDBSaver",
     "InMemoryAgentRegistry",
     "InMemoryMcpRegistry"
-
 ]
